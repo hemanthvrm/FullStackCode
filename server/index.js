64c9ebd4f62a5d61af1a66ac3750d2a69c1b93fa
Hemanth <hemanthvrm@gmail.com>
2019-05-02 00:14:30 -0500

    google auth and cookie setup/connected to mongo db

--- server/index.js
+++ server/index.js
@@ -1,8 +1,27 @@
 const express = require("Express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+const bodyParser = require("body-parser");
+const keys = require("./config/envKeys");
+require("./models/User");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
+app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    maxAge: 1 * 8 * 60 * 60 * 1000, // 8 Hours
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+require("./routes/demoRoutes")(app);
+require("./routes/authRoutes")(app);
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
